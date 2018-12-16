38fb8caf2cb395c3df2274fd0651b51c621a5723
Theriley106 <christopherlambert106@gmail.com>
2018-12-15 21:34:15 -0500

    Added  session ids

--- app.py
+++ app.py
@@ -2,36 +2,46 @@ from flask import Flask, render_template, request, url_for, redirect, Markup, js
 import scramble
 import csv
 import os
-DB_FILE = "static/database.csv"
+import random
+
+DB_FILE = "static/{0}.csv"
 app = Flask(__name__, static_url_path='/static')
-SCRAMBLES = []
+SCRAMBLES = {}
 
-
-if (os.path.exists(DB_FILE) == False):
-	with open(DB_FILE,'a') as fd:
-		writer = csv.writer(fd)
-		writer.writerow(["Scramble", "Time"])
+def newSession():
+	return "".join([str(random.randint(1,10)) for i in range(10)])
 
 @app.route('/', methods=['GET'])
 def index():
+	sessionID = newSession()
+	return redirect(url_for('mySession', sessionID=sessionID))
+
+@app.route('/session/<sessionID>', methods=['GET'])
+def mySession(sessionID):
+	if (os.path.exists(DB_FILE.format(sessionID)) == False):
+		with open(DB_FILE.format(sessionID),'a') as fd:
+			writer = csv.writer(fd)
+			writer.writerow(["Scramble", "Time"])
 	return render_template("index.html")
 
-@app.route('/genNew', methods=['GET'])
-def newScramble():
+@app.route('/genNew/<sessionID>', methods=['GET'])
+def newScramble(sessionID):
 	scrambleVal = str(scramble.genNew(25))
-	SCRAMBLES.append(scrambleVal)
+	if sessionID not in SCRAMBLES:
+		SCRAMBLES[sessionID] = []
+	SCRAMBLES[sessionID].append(scrambleVal)
 	return scrambleVal
 
-@app.route("/complete/<timeVal>")
-def submitTime(timeVal):
-	scrambleVal = SCRAMBLES.pop(-1)
+@app.route("/complete/<sessionID>/<timeVal>")
+def submitTime(sessionID, timeVal):
 	try:
+		scrambleVal = SCRAMBLES[sessionID].pop(-1)
 		timeVal = float(timeVal.replace("E", "."))
+		with open(DB_FILE.format(sessionID),'a') as fd:
+			writer = csv.writer(fd)
+			writer.writerow([scrambleVal, timeVal])
 	except:
-		timeVal = 0.0
-	with open(DB_FILE,'a') as fd:
-		writer = csv.writer(fd)
-		writer.writerow([scrambleVal, timeVal])
+		pass
 	return ""
 
 if __name__ == '__main__':
